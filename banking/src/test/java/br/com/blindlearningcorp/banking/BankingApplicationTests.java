6a451ac5bc6516d20d01d620c167bb6adabea5d2
leiTos0 <ailton.leite2000@gmail.com>
2021-12-28 15:41:22 -0300

    Projeto Spring Boot

---
+++ banking/src/test/java/br/com/blindlearningcorp/banking/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.blindlearningcorp.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
