1eb0223824b025a54226e01b4ab94aefcf4f889e
leiTos0 <ailton.leite2000@gmail.com>
2022-01-08 17:04:09 -0300

    Commit final

--- banking/src/main/java/br/com/blindlearningcorp/banking/controller/DadosController.java
+++ banking/src/main/java/br/com/blindlearningcorp/banking/controller/DadosController.java
@@ -1,13 +1,12 @@
 package br.com.blindlearningcorp.banking.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.blindlearningcorp.banking.dao.DadosDao;
 import br.com.blindlearningcorp.banking.model.Dados;
@@ -22,14 +21,18 @@ public class DadosController {
 	
 	// Metodo que exibe todos os dados, com o endpoint /contas de metodo GET
 	@RequestMapping(value = "/contas", method = RequestMethod.GET)
-	public List<Dados> exibirDados(){
-		return (List<Dados>)dados.findAll();
+	public ModelAndView exibirDados() {
+		ModelAndView mv = new ModelAndView("html/bank");
+		return mv.addObject("conta", dados.findAll());
 	}
 	
-	// Metodo que exibe um dado especifico, buscando pelo atributo numero
 	@RequestMapping(value = "/contas/{numero}", method = RequestMethod.GET)
-	public Dados buscarDado(@PathVariable int numero) {
-		return dados.findById(numero).orElse(null);
+	public ModelAndView buscarDado(@PathVariable int numero) {
+		ModelAndView mv = new ModelAndView("html/search");
+		if(numero == 0) {
+			return mv.addObject("busca", dados.findAll());
+		}
+		return mv.addObject("busca", dados.findById(numero).orElse(null));
 	}
 	
 	// Deleta um dado especifico, buscando pelo atributo numero
@@ -40,7 +43,8 @@ public class DadosController {
 	
 	// Adiciona um novo dado pelo metodo HTTP POST
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public void adicionarDado(@RequestBody Dados newConta) {
-		dados.save(newConta);
+	public ModelAndView adicionarDado(Dados newConta) {
+		ModelAndView mv = new ModelAndView("html/home");
+		return mv.addObject("conta", dados.save(newConta));
 	}
 }
